66c3fa1e6987c3eec607da2656f5f20bf0856ca5
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-11-11 22:52:23 +0000

    Cards.jsx: Set up api link. Currently links to specific pokemon. Set up useState variable data. Wrote async function for fetching image from api. Need to make it take input

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,29 @@
 import './Cards.css'
+import { useState, useEffect } from 'react';
 
 function Cards() {
 
+    const link = 'https://pokeapi.co/api/v2/pokemon/mewtwo'
+
+    const pokemon = ["ditto", "charizard", "squirtle", "pikachu", "magnemite", "arbok", "venusaur", "machamp", "zapdos", "mewtwo"]
+
+    const [data, setData] = useState(null);
+
+    async function getImage() {
+        try {
+            const res = await fetch(link);
+            const json = await res.json();
+            setData(json.sprites.other['official-artwork'].front_default);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <>
+
+        </>
+    )
 }
 
 export default Cards;
