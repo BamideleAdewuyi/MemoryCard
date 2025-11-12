21a19e47709200a1637cca302cd8bcc34d4baea7
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-11-12 00:22:48 +0000

    Cards.jsx: Set up pokemonList as a state variable. data is now image. async function is now getImage

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,25 +3,29 @@ import { useState, useEffect } from 'react';
 
 function Cards() {
 
-    const link = 'https://pokeapi.co/api/v2/pokemon/mewtwo'
+    const link = 'https://pokeapi.co/api/v2/pokemon/'
 
-    const pokemon = ["ditto", "charizard", "squirtle", "pikachu", "magnemite", "arbok", "venusaur", "machamp", "zapdos", "mewtwo"]
+    const [pokemonList, setPokemonList] = useState(["ditto", "charizard", "squirtle", "pikachu", "magnemite", "arbok", "venusaur", "machamp", "zapdos", "mewtwo"])
 
-    const [data, setData] = useState(null);
+    const [image, setImage] = useState(null);
 
-    async function getImage() {
+    async function getImage(pokemon) {
         try {
-            const res = await fetch(link);
+            const res = await fetch(link+pokemon);
             const json = await res.json();
-            setData(json.sprites.other['official-artwork'].front_default);
+            setImage(json.sprites.other['official-artwork'].front_default);
         } catch (error) {
             console.error(error)
         }
     }
 
+    useEffect(() => {
+        getImage("mew")
+    }, [])
+
     return (
         <>
-
+            {image ? <img src={image}/> : "Loading"}
         </>
     )
 }
