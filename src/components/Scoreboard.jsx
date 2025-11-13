a5286c6970ce8c90cff7506b849ef7297c326f4d
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-11-13 14:28:49 +0000

    Scoreboard.jsx: Returns high score and current score

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,7 +1,13 @@
 import './Scoreboard.css'
 
-function Scoreboard() {
-
+function Scoreboard({currentScore, highScore}) {
+    return(
+        <div className='scoreboad'>
+            <h3 className='highScore'>High score: {highScore}</h3>
+            <h3>Current score: {currentScore}</h3>
+        </div>
+    )
+    
 }
 
 export default Scoreboard
