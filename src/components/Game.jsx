dacaf553779ba4d9d405b20a153cf6c18b54852a
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-11-13 23:04:29 +0000

    Game.jsx: Wrote updateScores function. Passed into Grid component. Works like example, though there is no "winning" of game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,12 +7,28 @@ function Game() {
     const [currentScore, setCurrentScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
     const [guessed, setGuessed] = useState([]);
+
+    function updateScores(name) {
+        if (!guessed.includes(name)) {
+            setGuessed([...guessed, name])
+            setCurrentScore(currentScore + 1)
+        } else {
+            if (currentScore > highScore) {
+                setHighScore(currentScore)
+                setCurrentScore(0)
+                setGuessed([])
+            }
+        }
+    }
+
     return (
         <>
         <Scoreboard
         currentScore={currentScore}
         highScore={highScore}/>
-        <Grid/>
+        <Grid
+        stateFunction={updateScores}
+        state={currentScore}/>
         </>
     )
 }
