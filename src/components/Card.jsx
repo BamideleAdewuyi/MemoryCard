9e7b7222cfbdb17d58bd784c20b6624039649d74
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-11-13 01:56:01 +0000

    Card.jsx: returns a div with image and label for image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,12 @@
 import './Card.css'
 
 function Card(name, url) {
-
+    return(
+        <div className='pokemonCard'>
+            <img src={url} alt={name} className='pokemonImage'/>
+            <h4 className='imageLabel'>{name}</h4>
+        </div>
+    );
 };
 
 export default Card;
