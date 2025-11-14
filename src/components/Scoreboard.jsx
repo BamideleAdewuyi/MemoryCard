049143dedad3131ad4327bddeac39c73ed46105b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-11-14 00:43:54 +0000

    Scoreboard.jsx: Corrected spelling of scoreboard classname

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -2,7 +2,7 @@ import './Scoreboard.css'
 
 function Scoreboard({currentScore, highScore}) {
     return(
-        <div className='scoreboad'>
+        <div className='scoreboard'>
             <h3 className='highScore'>High score: {highScore}</h3>
             <h3>Current score: {currentScore}</h3>
         </div>
